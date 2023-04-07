bf8d2eff3582d1f507cd06678bb2a99da796793e
kondokentaro <kondokentaro@gmail.com>
2023-04-07 22:29:26 +0900

    add handler to sp menu

--- components/layout/adminMenus.tsx
+++ components/layout/adminMenus.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/navigation';
+import { deleteCookie } from 'cookies-next';
 import LogoutIcon from '@mui/icons-material/Logout';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 export interface IAdminMenus {
@@ -5,6 +7,15 @@ export interface IAdminMenus {
 }
 
 const AdminMenus = ({ listPage }: IAdminMenus) => {
+  const router = useRouter();
+  const handleAgenda = () => {
+    router.push('/administrator');
+  };
+  const handleLogout = () => {
+    deleteCookie('token');
+    router.push('/auth/login');
+  };
+
   return (
     <>
       <div className='h-[94px] px-[60px] pb-[25px] bg-[#808080] sm:hidden flex justify-between items-center'>
@@ -12,11 +23,12 @@ const AdminMenus = ({ listPage }: IAdminMenus) => {
           className={`${
             listPage ? 'text-[#fff]' : 'text-[#fff]/[0.5]'
           } flex flex-col items-center gap-1`}
-        >
+          onClick={handleAgenda}>
           <CalendarMonthIcon />
           <p className='text-xs'>アジェンダ一覧</p>
         </div>
-        <div className='text-[#fff]/[0.5] flex flex-col items-center gap-1'>
+        <div className='text-[#fff]/[0.5] flex flex-col items-center gap-1'
+          onClick={handleLogout}>
           <LogoutIcon />
           <p className='text-xs'>ログアウト</p>
         </div>
