4c0f66b042966eafb80923e4ab5c30094f3a8652
KONDO Kentaro <kentarokondo@macbook-pro.local>
2023-05-28 13:15:23 +0900

    yarn format

--- components/loader/index.tsx
+++ components/loader/index.tsx
@@ -1,10 +1,10 @@
 export default function Loader() {
   return (
     <>
-      <div className="flex items-center justify-center py-20">
-        <div className="animate-ping h-2 w-2 bg-[#BA00FF] rounded-full"></div>
-        <div className="animate-ping h-2 w-2 bg-[#BA00FF] rounded-full mx-4"></div>
-        <div className="animate-ping h-2 w-2 bg-[#BA00FF] rounded-full"></div>
+      <div className='flex items-center justify-center py-20'>
+        <div className='animate-ping h-2 w-2 bg-[#BA00FF] rounded-full'></div>
+        <div className='animate-ping h-2 w-2 bg-[#BA00FF] rounded-full mx-4'></div>
+        <div className='animate-ping h-2 w-2 bg-[#BA00FF] rounded-full'></div>
       </div>
     </>
   );
